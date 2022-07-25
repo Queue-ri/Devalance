271c41985948282c0c193f873f105ece143c4d39
Queue-ri <qriositylog@gmail.com>
2022-07-25 12:51:42 +0900

    chore: Add Spring Boot templates

---
+++ backend/src/main/java/com/example/devalance/DevalanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.devalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevalanceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/devalance/DevalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
