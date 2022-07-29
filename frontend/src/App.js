0528b71bebd728bef864216f668a58493cc953e5
Queue-ri <qriositylog@gmail.com>
2022-07-29 14:37:56 +0900

    feat: Add MainPage & DevPage to `App.js` and more
    - remove default App css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import DevPage from './pages/DevPage';
+// import NewPage from './pages/NewPage';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => { document.body.style.backgroundColor = '#f0f1f6' }, [])
+
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/dev" element={<DevPage />} />
+                    {/* <Route exact path="/new" element={<NewPage />} /> */}
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
