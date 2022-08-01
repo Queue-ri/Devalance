cdc6d07e446e51c8d3bdfc1206b4e80e0e9d655a
Queue-ri <qriositylog@gmail.com>
2022-08-01 14:47:36 +0900

    feat: Add PlayPage, PopularPage & SelfPage to `App.js`

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,10 @@ import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import DevPage from './pages/DevPage';
+import SelfPage from './pages/SelfPage';
 import NewPage from './pages/NewPage';
+import PlayPage from './pages/PlayPage';
+import PopularPage from './pages/PopularPage';
 
 
 function App() {
@@ -13,8 +16,11 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route path="/" element={<MainPage />} />
+                    <Route path="/popular" element={<PopularPage />} />
                     <Route path="/dev" element={<DevPage />} />
+                    <Route path="/self" element={<SelfPage />} />
                     <Route exact path="/new" element={<NewPage />} />
+                    <Route path="/play/*" element={<PlayPage />} />
                 </Routes>
             </div>
         </BrowserRouter>
