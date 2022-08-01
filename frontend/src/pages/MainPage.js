99f8517e8afe1134c4b609004fa3935a0d349121
Queue-ri <qriositylog@gmail.com>
2022-08-01 14:45:26 +0900

    chore: Connect test data to MainPage

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -6,98 +6,12 @@ import InfiniteScroll from "react-infinite-scroller";
 import { Col, Container, Row } from "react-bootstrap";
 import SideBar from "../components/navigation/SideBar";
 import NavBar from "../components/navigation/NavBar";
+import test_res from "../test/mainpage_test_res.json"
 
 
 function MainPage() {
-    const test_res_1 = [
-        {
-            img: "https://media.istockphoto.com/vectors/little-baby-kitten-vector-id1221209216?k=20&m=1221209216&s=612x612&w=0&h=3GBULc0ZU1VYhmYXQV0dTIckV-faB7Q_Dg3GlxfHuso=",
-            title: "내가 고양이라면?",
-            content: "나와 똑 닮은 성격의 고양이 찾기",
-            likes: 9999
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 9999
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 9999
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 9999
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 9999
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 9999
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 9999
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 9999
-        },
-        {
-            img: "",
-            title: "✨Test Title",
-            content: "첫 데이터의 마지막 카드입니다.",
-            likes: 9999
-        }
-    ];
-
-    const test_res_2 = [
-        {
-            img: "",
-            title: "✨두 번째 데이터",
-            content: "데이터 불러오기 성공~",
-            likes: 1111
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 1234
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 1234
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 1234
-        },
-        {
-            img: "",
-            title: "Test Title",
-            content: "테스트용 데이터입니다.",
-            likes: 1234
-        }
-    ];
+    const test_res_1 = test_res["res_1"]
+    const test_res_2 = test_res["res_2"]
 
     const test_res_length = test_res_1.length + test_res_2.length;
 
@@ -122,7 +36,7 @@ function MainPage() {
             <Container fluid="md" style={{height: "100%", paddingTop: "40px"}}>
                 <Row>
                     <Col xs="3">
-                        <SideBar highlighted={1}/>
+                        <SideBar highlighted={1} />
                     </Col>
                     <Col xs>
                         <InfiniteScroll
@@ -138,6 +52,7 @@ function MainPage() {
                                 {cards?.map((x) => {
                                     return (
                                         <Card
+                                            id={x.id}
                                             img={x.img}
                                             title={x.title}
                                             content={x.content}
